b398680305f12cc15fd7bc0877f04bed7db4c851
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2024-03-11 08:04:55 +0530

    a

--- r/mScales.R
+++ r/mScales.R
@@ -1,3 +1,12 @@
 #measurement Scales
 
 #nominal, ordinal, interval, ratio
+
+#nominal
+colors = c('green','yellow','red')
+carColors = sample(colors, size=100, replace=T, prob=c(.3, .2, .5))
+carColors
+mean(carColors)
+median(carColors)
+
+library(DescTools)
