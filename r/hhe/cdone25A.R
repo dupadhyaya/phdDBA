76e9a79f5632c15657cef9c046b8312e142a02fd
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2025-04-01 11:30:10 +0530

    s

---
+++ r/hhe/cdone25A.R
@@ -0,0 +1,20 @@
+# HHE Classes
+
+pacman::p_load(tidyverse, googlesheets4, lubridate)
+# HHE Classes
+gsHC ='1-uy0H-mDDuTeQ5_412vK_i7pIiL0o9nOP_s2oDfjjE8'
+
+sheet_names(gsHC)
+cdone <- read_sheet(gsHC, sheet = "classportal",  skip=1, col_types = 'iDiDccccccccccccccc')
+head(cdone)
+s= parse_date_time(str_extract("8:00PM-10:00PM", "^[^-]+"), orders = "I:%M%p")
+e= parse_date_time(str_extract("8:00PM-10:00PM", "(?<=-).+$"), orders = "I:%M%p")
+(dmin = as.numeric(difftime(e, s, units = "mins")))
+
+names(cdone)
+(cdone2 <- cdone %>% mutate( start_time = parse_date_time(str_extract(classTiming, "^[^-]+"), orders = "I:%M%p"), end_time = parse_date_time(str_extract(classTiming, "(?<=-).+$"), orders = "I:%M%p"))  %>% mutate( dur_min = as.numeric(difftime(end_time, start_time, units = "mins")), pcount = as.integer(pcount)) %>% dplyr::select(cDate, cDay, batchID, batchSDate, course, classTiming, pcount, dur_min, claimed) %>% filter(is.na(claimed)))
+
+#summarise------
+cdoneS1 <- cdone2 %>% group_by(cDate) %>% summarise(n=n(), batcount = paste(batchID, collapse = ", "), duration = max(dur_min, na.rm=T), students = sum(pcount, na.rm=T)) %>% arrange(desc(cDate))
+hrs = sum(cdoneS1$duration)/60
+cat("Total hours of classes:", hrs, "\n and amount " , hrs*1500 , " INR")
