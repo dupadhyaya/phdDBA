90c46688399cbee65d2f5cb00a2f2c6506386004
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2025-06-01 22:12:02 +0530

    a

---
+++ r/dba/M3_hypothesis2.R
@@ -0,0 +1,8 @@
+#Hypothesis Testing in R
+
+# Data
+data <- c(12, 10, 15, 14, 18, 20, 11, 9, 17, 13)
+
+# Hypothesis test
+t.test(data, mu = 15) 
+# mu is the known value (population mean) you are comparing against
